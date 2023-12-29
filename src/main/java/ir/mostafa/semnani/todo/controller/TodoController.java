e65a826ebf78a6974dcd5bfdd585f20503eb6e72
Mostafa Semnani <mostafa.semnani.74@gmail.com>
2023-12-29 17:20:58 +0330

    add todo-react url address for cors in todo controller

--- src/main/java/ir/mostafa/semnani/todo/controller/TodoController.java
+++ src/main/java/ir/mostafa/semnani/todo/controller/TodoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/todos")
+@CrossOrigin("http://localhost:3000")
 @RequiredArgsConstructor
 public class TodoController {
     private final TodoService todoService;
